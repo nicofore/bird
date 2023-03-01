561fc32b1229596736f4ea28af588ef21cb63745
nfore <nicolas.fore@student.uclouvain.be>
2023-03-01 18:06:52 +0100

    t

--- nest/multi_test.c
+++ nest/multi_test.c
@@ -40,13 +40,13 @@ t_fib_simple(void){
 
     bt_assert_msg(pointer_to_a, "Failed to add node in empty fib\n"); //Check if pointer is not null
 
-    bt_assert_msg(net_equal_ip4(&(pointer_to_a->n.addr), &a), "Node received is not the node added\n");
+    bt_assert_msg(net_equal_ip4(&(pointer_to_a->n.addr), &a) == 0, "Node received is not the node added\n");
 
     pointer_to_a = fib_find(f, &a);
 
     bt_assert_msg(pointer_to_a, "Failed to find node which was added\n"); //Check if pointer is not null
 
-    bt_assert_msg(net_equal_ip4(&(pointer_to_a->n.addr), &a), "Node found is not the node added\n");
+    bt_assert_msg(net_equal_ip4(&(pointer_to_a->n.addr) == 0, &a), "Node found is not the node added\n");
 
     fib_free(f);
     rfree(lp);
@@ -83,7 +83,7 @@ static int t_fib_10000_address(void){
         net_addr_ip4 a = NET_ADDR_IP4(i, 32);
         net* entry = fib_find(f, &a);
         bt_assert_msg(entry, "Failed to find node %d in t_fib_10000_address\n", i);
-        bt_assert_msg(net_equal_ip4(&(entry->n.addr), &a), "Entry found is not the entry added\n");
+        bt_assert_msg(net_equal_ip4(&(entry->n.addr), &a) == 0, "Entry found is not the entry added\n");
         fib_delete(f, entry);
     }
 
@@ -106,7 +106,7 @@ main(int argc, char *argv[])
   bt_init(argc, argv);
 
   bt_test_suite(t_fib_simple, "Testing Simple operation fib");
-  bt_test_suite(t_fib_10000_address, "Testing Adding/get/remove operation  operation fib");
+  bt_test_suite(t_fib_10000_address, "Testing Adding/get/remove operation fib");
 
   return bt_exit_value();
 }
