7a0221cf156a0e16252e709685d3e88892e6f203
nfore <nicolas.fore@student.uclouvain.be>
2023-03-01 12:49:42 +0100

    test

---
+++ nest/multi_test.c
@@ -0,0 +1,21 @@
+#include "test/birdtest.h"
+#include "test/bt-utils.h"
+
+#include <stdio.h>
+
+
+
+
+
+
+
+
+int
+main(int argc, char *argv[])
+{
+  bt_init(argc, argv);
+
+  printf("Test was launched\n");
+
+  return 0;
+}
