74cfac7f784dceebafaf1a3bc46a81baa6d231cd
nfore <nicolas.fore@student.uclouvain.be>
2023-03-02 15:08:54 +0100

    t

--- nest/multi_test.c
+++ nest/multi_test.c
@@ -6,8 +6,16 @@
 
 #include <stdio.h>
 
+#include <pthread.h>
 
 
+
+
+
+typedef struct threadArgs{
+    struct fib* fib;
+    int threadNumber;
+} threadArgs;
 
 static int
 t_fib_simple(void){
@@ -39,9 +47,6 @@ t_fib_simple(void){
 
     pointer_to_a = fib_find(f, (net_addr*) &a);
 
-    if (pointer_to_a == NULL){
-        printf("Failed 2 to add node in empty fib\n");
-    }
 
     bt_assert_msg(pointer_to_a!= NULL, "Failed to find node which was added\n"); //Check if pointer is not null
 
@@ -97,7 +102,93 @@ static int t_fib_10000_address(void){
 }
 
 
+void* f_multi_Add(void* args){
 
+    threadArgs* args = (threadArgs*) argus;
+
+    int threadNumber = args->threadNumber;
+
+    for (int i = 0; i < 10000; i++){
+        net_addr_ip4 a = NET_ADDR_IP4(6*i + threadNumber, 32);
+        net* entry = fib_get(args->fib, (net_addr*) &a);
+        bt_assert_msg(entry, "Failed to add node %d in t_fib_10000_address\n", i);
+    }
+    
+    
+}
+
+void* f_multi_remove(void* args){
+
+    threadArgs* args = (threadArgs*) argus;
+
+    int threadNumber = args->threadNumber;
+
+    for (int i = 0; i < 10000; i++){
+        net_addr_ip4 a = NET_ADDR_IP4(6*i + threadNumber, 32);
+        net* entry = fib_find(args->fib, (net_addr*) &a);
+        bt_assert_msg(entry, "Failed to add node %d in t_fib_10000_address\n", i);
+        fib_delete(args->fib, entry);
+    }
+} 
+
+
+
+
+
+static int t_multi_thread(void){
+    resource_init(); //Initialize the root pool
+
+    struct fib *f = mb_alloc(&root_pool, sizeof(struct fib));
+
+    //Initialize the fib
+    fib_init(f, &root_pool, NET_IP4, sizeof(net), OFFSETOF(net, n), 0, NULL);
+
+    pthread_t threads[6];
+    threadArgs args[6];
+
+    for (int i = 0; i < 6; i++){
+        args[i].fib = f;
+        args[i].threadNumber = i;
+    }
+
+    //Create threads
+    for (int i = 0; i < 6; i++){
+        pthread_create(&threads[i], NULL, f_multi_Add, (void*) &args[i]);
+    }
+
+    for (int i = 0; i < 6; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+
+    //Check if every entry is in the fib
+
+    for (int i = 0; i < 60000; i++){
+        net_addr_ip4 a = NET_ADDR_IP4(i, 32);
+        net* entry = fib_find(f, (net_addr*) &a);
+        bt_assert_msg(entry, "Failed to find node %d in t_fib_10000_address\n", i);
+        bt_assert_msg(net_equal_ip4((net_addr_ip4*) &(entry->n.addr), &a), "Entry found is not the entry added\n");
+    }
+
+    bt_assert_msg(f->entries == 60000, "Fib count is not 60000\n");
+
+
+    //Remove every entry
+
+    for (int i = 0; i < 6; i++){
+        pthread_create(&threads[i], NULL, f_multi_remove, (void*) &args[i]);
+    }
+
+    for (int i = 0; i < 6; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    bt_assert_msg(f->entries == 0, "Fib count is not 0 after removing every entries\n");
+
+    fib_free(f);
+
+    return 1;
+}
 
 
 int
